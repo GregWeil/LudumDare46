c4cab54cb5fef2fdeaddc7561772c9b6f077bd58
Greg Weil <bailinbone15@gmail.com>
2020-04-18 14:16:35 -0400

    Added a reticle that you can move around while in the keep

--- Assets/Cannon/CannonController.cs
+++ Assets/Cannon/CannonController.cs
@@ -1,17 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CannonController : MonoBehaviour {
     private GameObject Keep;
+    private Vector2 Movement = Vector2.zero;
+    private Vector2 Velocity = Vector2.zero;
+
+    public GameObject Target;
+    public float MoveSpeed;
+    public float Acceleration;
+    public float MinRange;
+    public float MaxRange;
 
     void Start() {
         Keep = GameObject.Find("Keep");
     }
 
     void Update() {
-        transform.position = Keep.transform.position;
+        transform.position = new Vector3(Keep.transform.position.x, 0, Keep.transform.position.z);
+        Velocity = Vector2.MoveTowards(Velocity, Movement, Acceleration * Time.deltaTime);
+        Target.transform.localPosition += new Vector3(Velocity.x, 0, Velocity.y) * MoveSpeed * Time.deltaTime;
+        var distance = Target.transform.localPosition.magnitude;
+        if (distance < MinRange) {
+            Target.transform.localPosition = Target.transform.localPosition.normalized * MinRange;
+        } else if (distance > MaxRange) {
+            Target.transform.localPosition = Target.transform.localPosition.normalized * MaxRange;
+        }
     }
 
-    void OnTower() {
-        SendMessageUpwards("ExitKeep");
+    void OnMove(InputValue value) {
+        Movement = value.Get<Vector2>();
+    }
+
+    void OnTower(InputValue value) {
+        if (value.isPressed) {
+            SendMessageUpwards("ExitKeep");
+        }
     }
 }
