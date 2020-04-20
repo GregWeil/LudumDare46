ce61bdae46a7ffe0bbb46c44241fbf8f929e9671
Greg Weil <bailinbone15@gmail.com>
2020-04-20 18:30:14 -0400

    More progress on the orc

--- Assets/Orc/OrcMovement.cs
+++ Assets/Orc/OrcMovement.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class OrcMovement : MonoBehaviour {
     private Animator Animation;
+    private Vector3 Target;
 
     public float MoveSpeed;
+    public float AggroRange;
 
     void Start() {
         Animation = GetComponentInChildren<Animator>();
+        Target = transform.position;
+        StartCoroutine(Cycle());
+    }
+
+    IEnumerator Cycle() {
+        while (true) {
+            var target = GameObject.Find("Keep");
+            var players = GameObject.FindObjectsOfType<PlayerMovement>();
+            foreach (var player in players) {
+                var offset = player.transform.position - transform.position;
+                offset.y = 0;
+                if (offset.magnitude < AggroRange) {
+                    target = player.gameObject;
+                    break;
+                }
+            }
+            Target = target.transform.position;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.25f));
+        }
     }
 
     void Update() {
-        
+        var velocity = MoveSpeed * (Target - transform.position).normalized;
+        velocity.y = 0f;
+        Animation.SetFloat("Speed", velocity.magnitude);
+        transform.position += velocity * Time.deltaTime;
     }
 }
