deab39a5b3ec3ef6420b42428b99792d1dee7cd9
Greg Weil <bailinbone15@gmail.com>
2020-04-20 13:11:32 -0400

    Added an alternate blue player skin

---
+++ Assets/Character/Sprites/SpriteSwap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpriteSwap : MonoBehaviour {
+    [System.Serializable]
+    public class SwapEntry {
+        public Sprite Base;
+        public Sprite Override;
+    }
+
+    public SwapEntry[] Replacements;
+    private Dictionary<Sprite, Sprite> Lookup;
+    private SpriteRenderer Renderer;
+
+    void Start() {
+        Lookup = Replacements.ToDictionary(entry => entry.Base, entry => entry.Override);
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void LateUpdate() {
+        if (Lookup.ContainsKey(Renderer.sprite)) {
+            Renderer.sprite = Lookup[Renderer.sprite];
+        }
+    }
+}
