2252fcf062718b0e337246fae47cb6a7c3df4739
Greg Weil <bailinbone15@gmail.com>
2020-04-20 10:25:40 -0400

    Added a display with the keep's health and ammo

--- Assets/Keep/KeepController.cs
+++ Assets/Keep/KeepController.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class KeepController : MonoBehaviour {
+    public Text HealthDisplay;
+    public Text AmmoDisplay;
+
     public int Health;
     public int Ammo;
 
+    public float DamageTimeout;
+    private float LastDamage = 0f;
+
+    void Update() {
+        HealthDisplay.text = $"{Health}";
+        AmmoDisplay.text = $"{Ammo}";
+    }
+
     void Damage() {
-        Health -= 1;
+        if (Time.time >= LastDamage + DamageTimeout) {
+            Health -= 1;
+            LastDamage = Time.time;
+        }
     }
 }
