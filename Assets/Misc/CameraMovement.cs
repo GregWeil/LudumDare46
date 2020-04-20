c3db1fb56dbe520ed409bbd472bb232e1c3b5eb7
Greg Weil <bailinbone15@gmail.com>
2020-04-19 21:02:20 -0400

    The camera tracks the keep and any players

---
+++ Assets/Misc/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public GameObject Target;
+    public float MoveSpeed;
+    public float PlayerWeight;
+
+    void Update() {
+        if (Target != null) {
+            var targetPosition = Target.transform.position;
+            var players = GameObject.FindGameObjectsWithTag("Player");
+            if (players.Any()) {
+                var playersPosition = players.Aggregate(Vector3.zero, (position, player) => position + player.transform.position);
+                targetPosition += (playersPosition - targetPosition) * PlayerWeight;
+            }
+            targetPosition.y = 0f;
+            var lerp = 1f - Mathf.Pow(1f - MoveSpeed, Time.deltaTime*60f);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, lerp);
+        }
+    }
+}
