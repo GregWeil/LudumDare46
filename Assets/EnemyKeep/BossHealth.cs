942f77214dc4f10353e7f3f9d3e4f2d520d96738
Greg Weil <bailinbone15@gmail.com>
2020-04-20 11:25:19 -0400

    Enemy bosses have health and can be destroyed

---
+++ Assets/EnemyKeep/BossHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour {
+    private Vector3 ShakeOffset = Vector3.zero;
+    private float ShakeTimer = 0f;
+
+    public EnemyCannon[] Cannons;
+
+    public int Health;
+    public float FallSpeed;
+    public float ShakeInterval;
+    public float ShakeRadius;
+
+    void Update() {
+        if (Health <= 0) {
+            foreach (var cannon in Cannons) {
+                cannon.CooldownTime = float.PositiveInfinity;
+            }
+
+            if (ShakeTimer < 0f) {
+                transform.Translate(-ShakeOffset);
+                ShakeOffset = Random.insideUnitSphere * ShakeRadius;
+                transform.Translate(ShakeOffset);
+                ShakeTimer = ShakeInterval;
+            }
+            ShakeTimer -= Time.deltaTime;
+
+            transform.Translate(0, -FallSpeed * Time.deltaTime, 0);
+            if (transform.position.y < -15f) Destroy(gameObject);
+        }
+    }
+
+    void Damage() {
+        Health -= 1;
+    }
+}
--- Assets/Keep/KeepController.cs
+++ Assets/Keep/KeepController.cs
@@ -8,18 +8,12 @@ public class KeepController : MonoBehaviour {
     public int Health;
     public int Ammo;
 
-    public float DamageTimeout;
-    private float LastDamage = 0f;
-
     void Update() {
         HealthDisplay.text = $"{Health}";
         AmmoDisplay.text = $"{Ammo}";
     }
 
     void Damage() {
-        if (Time.time >= LastDamage + DamageTimeout) {
-            Health -= 1;
-            LastDamage = Time.time;
-        }
+        Health -= 1;
     }
 }
