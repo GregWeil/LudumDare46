6bdab5173f27118feb54538260928c35634eff0c
Greg Weil <bailinbone15@gmail.com>
2020-04-20 13:59:12 -0400

    Multiple players can join the game

---
+++ Assets/Controller/PlayerJoin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJoin : MonoBehaviour {
+    public GameObject[] CharacterPrefabs;
+    private int JoinIndex = 0;
+
+    void OnPlayerJoined(PlayerInput player) {
+        player.gameObject.name = $"Player {JoinIndex + 1}";
+        player.GetComponent<PlayerController>().CharacterPrefab = CharacterPrefabs[JoinIndex % CharacterPrefabs.Length];
+        JoinIndex += 1;
+    }
+}
