5ec2a0ad2c8bd370821db22bdea8d2006cabebf2
Greg Weil <bailinbone15@gmail.com>
2020-04-20 14:14:47 -0400

    Added a fourth purple player

---
+++ Assets/Controller/EnemyKeepSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKeepSpawner : MonoBehaviour {
+    public GameObject EnemyKeepPrefab;
+    private GameObject ActiveKeep = null;
+
+    void Update() {
+        if (ActiveKeep == null) {
+
+        }
+    }
+
+    void SpawnKeep() {
+        
+    }
+}
